f7b1d8b6f1b9770b3a05fed8a13aa4e6c19f133d
aliciariveraa <aliciarivera@usf.edu>
2025-03-30 15:52:29 -0400

    Task 3: Add fetchProductsAsync using async/await and try/catch

--- cc_16.js
+++ cc_16.js
@@ -16,3 +16,25 @@ function fetchProductsThen() {
   
   // Call the function to check if it works
   fetchProductsThen();  
+
+
+  // Task 3: Add fetchProductsAsync using async/await and try/catch
+
+  async function fetchProductsAsync() {
+    try {
+      const response = await fetch('https://www.course-api.com/javascript-store-products');
+      const products = await response.json();
+      
+      // Call the display function after fetching the products
+      displayProducts(products);
+    } catch (error) {
+      handleError(error);
+    }
+  }
+  
+  // Call fetchProductsAsync
+  fetchProductsAsync();
+  
+
+
+
